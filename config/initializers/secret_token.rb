5bfda6cfe3e642ca8ae22c1cfa35fe9cfbd62f8a
Michael Scriven <bigscriv03@gmail.com>
2014-01-14 00:02:30 +0000

    Basic Rails and Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'eed484487f17adb341cd9c7452e6509932291cffbdda1db8b7b9901a7f6f037832cd302b16a5624fcaebb3d62420b8b7a51828881b909e186db9939967960584'
